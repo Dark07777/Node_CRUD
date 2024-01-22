752089c3fd4da8eacf227201d7d4d0baab152130
Node_CRUD <gegham.shagoyan.y@tumo.org>
2024-01-22 21:24:34 +0400

    connect to db

--- public/script.js
+++ public/script.js
@@ -1,13 +1,14 @@
-let x = document.querySelector('.btn')
+let x = document.querySelector(".btn");
 
-x.addEventListener('click',()=>{
-    let inp = document.querySelector('.inp').value
-let inp1 = document.querySelector('.inp1').value
-    fetch('/addName',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body:JSON.stringify({name: inp, age:inp1 })
-    })         
-})
+x.addEventListener("click", () => {
+  let usName = document.querySelector(".usName").value;
+  let usAge = document.querySelector(".usAge").value;
+  let usEmail = document.querySelector(".usEmail").value;
+  fetch("http://localhost:7777/addName", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: usName, age: usAge,email:usEmail }),
+  });
+});
--- server.js
+++ server.js
@@ -1,68 +1,63 @@
-// var express = require("express");
-// var app = express();
-// let path = require('path')
-// let bodyParser = require('body-parser')
-
-// app.use(bodyParser.urlencoded({extended: true}))
-// app.use(bodyParser.json())
-// app.use(express.static('public'))
-// app.get("/", function(req, res){
-
-//     res.sendFile(path.join(__dirname,'./public/form.html'));
-// })
-// app.post('/addName', function(req, res){
-    
-//     let name= req.body.name
-//     let age = req.body.age
-//     console.log(name,age)
-//     res.redirect('/')
-// });
-
-// app.listen(7777, function(){
-//    console.log("Example is running on port 7777");
-// });
-
-
+var express = require("express");
+var app = express();
+let path = require('path')
+let bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-
-// Replace the connection string with your MongoDB connection string
-const connectionString = 'mongodb+srv://geghamshagoyany:chax777@cluster0.zfgsp1x.mongodb.net/?retryWrites=true&w=majority';
-
-
-// Connect to MongoDB
+const connectionString = 'mongodb+srv://geghamshagoyany:chax777@cluster0.zfgsp1x.mongodb.net/sample_mflix';
 mongoose.connect(connectionString, { useUnifiedTopology: true });
-
-// Check the connection
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
 db.once('open', async () => {
-    console.log('Connected to MongoDB!');
+  console.log('Connected to MongoDB!');
+
+  try {
+    app.post('/addName', async function (req, res){
+    
+      let name= req.body.name
+      let age = req.body.age
+      console.log(name,age)
+      res.redirect('/')
+var result = await mongoose.connection.db.collection('users').insertMany({ name: "Chax", email: 'Chaxjan@mail.ru',age:420} )
+  console.log(result);  
+}
   
-    try {
-      // Retrieve data from the Person collection
-      const people = await Person.find(); // Retrieve all documents
   
-      console.log('Retrieved data:', people);
+  );
+
+
     } catch (error) {
-      console.error('Error retrieving data:', error);
+    console.error('Error retrieving customers:', error);
     } finally {
-      // Make sure to close the connection when you're done
-      mongoose.connection.close();
+    mongoose.connection.close();
     }
   });
 
-// Define a Mongoose schema and model for the Person collection
+app.get("/", function(req, res){
+
+    res.sendFile(path.join(__dirname,'./public/form.html'));
+})
+
+
+app.listen(7777, function(){
+   console.log("Example is running on port 7777");
+});
+
+
+
+
 const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
+name: String,
+age: Number,
 });
 
 const Person = mongoose.model('Person', personSchema);
 
-// Replace the connection string with your MongoDB connection string
 
-// Connect to MongoDB
+
+
 mongoose.connect(connectionString);
 
-// Check the connection
