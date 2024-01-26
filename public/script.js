ae55a6a90007b8224f746dd0a5bf92f705248799
Dark07777 <gegham.shagoyan.y@tumo.org>
2024-01-26 21:25:56 +0400

    take from db

--- public/script.js
+++ public/script.js
@@ -4,11 +4,11 @@ x.addEventListener("click", () => {
   let usName = document.querySelector(".usName").value;
   let usAge = document.querySelector(".usAge").value;
   let usEmail = document.querySelector(".usEmail").value;
-  fetch("http://localhost:7777/addName", {
+  fetch("/addName", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ name: usName, age: usAge,email:usEmail }),
+    body: JSON.stringify({ name: usName, age: usAge, email:usEmail }),
   });
 });
--- server.js
+++ server.js
@@ -1,63 +1,57 @@
 var express = require("express");
+var path = require("path");
+const bodyParser = require('body-parser');
 var app = express();
-let path = require('path')
-let bodyParser = require('body-parser')
+
 const mongoose = require('mongoose');
-const connectionString = 'mongodb+srv://geghamshagoyany:chax777@cluster0.zfgsp1x.mongodb.net/sample_mflix';
-mongoose.connect(connectionString, { useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
+const connectionString = 'mongodb+srv://geghamshagoyany:geghamshagoyany@cluster0.zfgsp1x.mongodb.net/sample_mflix';
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(express.static('public'))
-db.once('open', async () => {
-  console.log('Connected to MongoDB!');
-
-  try {
-    app.post('/addName', async function (req, res){
-    
-      let name= req.body.name
-      let age = req.body.age
-      console.log(name,age)
-      res.redirect('/')
-var result = await mongoose.connection.db.collection('users').insertMany({ name: "Chax", email: 'Chaxjan@mail.ru',age:420} )
-  console.log(result);  
-}
-  
-  
-  );
+ mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        console.log('Connected to MongoDB!');
+        try {
+           let result = await mongoose.connection.db.collection('theaters').find({theaterId:1000})
 
 
-    } catch (error) {
-    console.error('Error retrieving customers:', error);
-    } finally {
-    mongoose.connection.close();
-    }
-  });
+    res.render('../public/form.ejs',{
+        obj:result
+      })
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    })
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.set('view engine','ejs')
 app.get("/", function(req, res){
+  let info =[
+      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+  { name: 'Tux', organization: "Linux", birth_year: 1996},
+  { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ]
 
-    res.sendFile(path.join(__dirname,'./public/form.html'));
-})
 
 
-app.listen(7777, function(){
-   console.log("Example is running on port 7777");
 });
 
+app.post('/addName', async (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const email = req.body.email;
+
+ });
 
 
 
-const personSchema = new mongoose.Schema({
-name: String,
-age: Number,
+
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
 });
-
-const Person = mongoose.model('Person', personSchema);
-
-
-
-
-mongoose.connect(connectionString);
-
